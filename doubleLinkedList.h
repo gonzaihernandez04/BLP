a166aa2da1649d082cc30a1960585bc19128fd36
Gonzalo <hernandezgonzalo5845@gmail.com>
2025-06-02 16:43:48 -0300

    feat: Se crea estructura base para DoubleLinked de estudiante y Materia. Se crea funcionalidad de cargar estudiante con validaciones correspondientes. Se cambian todas las funciones de la DDL para Estudiante. Se crea funcion para cargar materias del Plan. Se crea funcionalidad general para main

---
+++ doubleLinkedList.h
@@ -0,0 +1,209 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Materia{
+    char nombreMateria[55];
+    int inscripto;
+    int aprobada; //Indica si aprobo la materia
+    int firstTest;
+    int secondTest;
+
+} Materia;
+
+typedef struct Estudiante{
+    char nombre[45];
+    char nacimiento[10];
+    int dni;
+    int legajo;
+    Materia materias [48]; //Hasta 48 materias. POR DEFAULT
+
+} Estudiante;
+
+
+
+Materia *newMateria(char nombreMateria[55]){
+    Materia *materia = (Materia *)malloc(sizeof(Materia));
+    strcpy(materia->nombreMateria, nombreMateria); //  Copia segura de string
+      materia->inscripto = 0;
+    materia->aprobada = 0;
+    materia->firstTest = 0;
+    materia->secondTest = 0;
+    return materia;
+}
+
+typedef struct nodeDL
+{
+    Estudiante *estudiante;
+    struct nodeDL *next;
+    struct nodeDL *prev;
+} nodeDL;
+
+typedef struct
+{
+    nodeDL *head;
+    nodeDL *tail;
+    int size;
+} doubleLinkedList;
+
+// Crear nuevo nodo
+nodeDL *newNode(Estudiante *estudiante)
+{
+    nodeDL *nd = (nodeDL *)malloc(sizeof(nodeDL));
+    if (nd == NULL)
+        return NULL;
+    nd->estudiante = estudiante;
+    nd->next = NULL;
+    nd->prev = NULL;
+    return nd;
+}
+
+// Insertar nodo al inicio de la lista
+void prepend(doubleLinkedList *lista, Estudiante *estudiante)
+{
+    nodeDL *nodeNew = newNode(estudiante);
+    if (lista->size == 0)
+    {
+        lista->head = nodeNew;
+        lista->tail = nodeNew;
+    }
+    else
+    {
+        nodeNew->next = lista->head;
+        lista->head->prev = nodeNew;
+        lista->head = nodeNew;
+    }
+    lista->size++;
+}
+
+// Insertar nodo al final de la lista
+void append(doubleLinkedList *lista, Estudiante *estudiante)
+{
+    nodeDL *nodeNew = newNode(estudiante);
+    if (lista->size == 0)
+    {
+        lista->head = nodeNew;
+        lista->tail = nodeNew;
+    }
+    else
+    {
+        nodeNew->prev = lista->tail;
+        lista->tail->next = nodeNew;
+        lista->tail = nodeNew;
+    }
+    lista->size++;
+}
+
+// Imprimir la lista
+void printList(doubleLinkedList *lista)
+{
+    nodeDL *node = lista->head;
+    if (node != NULL)
+    {
+        printf("Lista: ");
+        while (node != NULL)
+        {
+            if (node->next != NULL)
+            {
+                printf("%s->", node->estudiante->nombre);
+            }
+            else
+            {
+                printf("%s\n", node->estudiante->nombre);
+            }
+
+            node = node->next;
+            
+        }
+    }
+    else
+    {
+        printf("Lista vacia. \n");
+    }
+}
+
+// Limpiar la lista
+void clear(doubleLinkedList *lista)
+{
+    nodeDL *current = lista->head;
+    while (current != NULL)
+    {
+        nodeDL *next = current->next;
+        free(current);
+        current = NULL;
+        current = next;
+    }
+    lista->head = NULL;
+    lista->tail = NULL;
+    lista->size = 0;
+    printf("La lista se vacio");
+}
+
+// Borrar nodo de la lista
+void deleteNode(Estudiante *estudiante, doubleLinkedList *lista)
+{
+
+    nodeDL *node = lista->head;
+    if (lista->size > 0) // Verificamos que la lista o el dato no esten vacios
+    {
+        while (node != NULL) // Mientras  llegue a la cola y el siguiente a el sea nulo
+        {
+
+            if (node->estudiante == estudiante)
+            {
+                if (node->prev != NULL)
+                {
+                    node->prev->next = node->next;
+                }
+                else
+                {
+                    lista->head = node->next; // Es el primer nodo
+                }
+                if (node->next != NULL)
+                {
+
+                    node->next->prev = node->prev;
+                }
+                else
+                {
+                    lista->tail = node->prev; // Es el último nodo
+                }
+
+                free(node); // libero memoria en nodo ya que encontro el valor y lo elimino
+                node = NULL;
+                lista->size--;
+                printf("Valor eliminado con exito");
+                return;
+            }
+
+            node = node->next;
+        }
+    }
+    printf("Nodo con valor %d no encontrado.\n", estudiante);
+}
+
+nodeDL *findNode(Estudiante *estudiante, doubleLinkedList *lista)
+{
+    nodeDL *node = lista->head;
+    if (lista->size > 0)
+    {
+
+        while (node != NULL)
+        {
+            if (node->estudiante == estudiante)
+            {
+                printf("El valor del nodo encontrado: %d ", node->estudiante);
+                return node;
+            }
+
+            node = node->next;
+        }
+    }
+    printf("Valor del nodo no encontrado");
+    return NULL;
+}
+
+
+   
+
--- main.c
+++ main.c
@@ -1,250 +1,292 @@
+#ifndef DOUBLE_LINKED_LIST_H
+#define DOUBLE_LINKED_LIST_H
+
+#include "doubleLinkedList.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-//Estructura del Node Double Linked
-typedef struct nodeDL
+#define CANT_ESTUDIANTES 10
+
+///  //////////////////////////////// CARGAR MATERIAS //////////////////////////////////////
+char *nombres[] = {"Algebra 1", "AyP1", "AyP2", "Base de Datos", "Historia","AYP3","Materia Discreta","Disenio Local"};
+//Defino una constante para la cantidad de materias para sumar materias nuevas sin modificar el codigo
+#define CANT_MATERIAS (sizeof(nombres) / sizeof(nombres[0]))
+doubleLinkedList lista = {NULL, NULL, 0};
+Materia *materias[CANT_MATERIAS];
+
+void cargarMaterias()
 {
-    int date; //Dato del nodo
-    struct nodeDL *next; //Siguiente al Nodo
-    struct nodeDL *prev; //Previo al Nodo
-} nodeDL;
 
-//Estructura de Double Linked List
-typedef struct
-{
-    nodeDL *head; //Cabeza de la lista
-    nodeDL *tail; //Cola de la lista
-    int size;
-} doubleLinkedList;
-
-// Crear nuevo nodo
-nodeDL *newNode(int date)
-{
-    nodeDL *nd = (nodeDL *)malloc(sizeof(nodeDL));
-    if (nd == NULL)
-        return NULL;
-    nd->date = date; 
-    nd->next = NULL;
-    nd->prev = NULL;
-    return nd;
-}
-
-// Insertar nodo al inicio de la lista
-void prepend(doubleLinkedList *lista, int date)
-{
-    nodeDL *nodeNew = newNode(date);
-    if (lista->size == 0) //Lista vacia
+    for (int i = 0; i < CANT_MATERIAS; i++)
     {
-        lista->head = nodeNew;
-        lista->tail = nodeNew;
-    }
-    else
-    {
-        nodeNew->next = lista->head;
-        lista->head->prev = nodeNew;
-        lista->head = nodeNew;
-    }
-    lista->size++;
-}
-
-// Insertar nodo al final de la lista
-void append(doubleLinkedList *lista, int date)
-{
-    nodeDL *nodeNew = newNode(date);
-    if (lista->size == 0) //Si esta vacia, el nodo ingresado va ser la cabeza y cola.
-    {
-        lista->head = nodeNew;
-        lista->tail = nodeNew;
-    }
-    else
-    {
-        nodeNew->prev = lista->tail; 
-        lista->tail->next = nodeNew;
-        lista->tail = nodeNew;
-    }
-    lista->size++;
-}
-
-// Imprimir la lista
-void printList(doubleLinkedList *lista)
-{
-    nodeDL *node = lista->head;
-    if (node != NULL) 
-    {
-        printf("Lista: ");
-        while (node != NULL)
-        {
-            if (node->next != NULL)
-            {
-                printf("[%d]->", node->date);
-            }
-            else
-            {
-                printf("[%d]", node->date);
-            }
-
-            node = node->next;
-            
-        }
-    }
-    else
-    {
-        printf("Lista vacia. \n");
+        materias[i] = malloc(sizeof(Materia)); // Reserva memoria para cada materia
+        strcpy(materias[i]->nombreMateria, nombres[i]);
+        materias[i]->inscripto = 0;
+        materias[i]->aprobada = 0;
+        materias[i]->firstTest = 0;
+        materias[i]->secondTest = 0;
     }
 }
 
-// Limpiar la lista
-void clear(doubleLinkedList *lista)
-{
-    nodeDL *current = lista->head;
-    while (current != NULL) //Mientras que el actual no sea null 
-    {
-        nodeDL *next = current->next; //Guardamos el nodo siguiente
-        free(current); //Liberamos el nodo 
-        current = NULL;
-        current = next; //Guardamos el siguiente como el actual
-    }
-    lista->head = NULL;
-    lista->tail = NULL;
-    lista->size = 0;
-    printf("La lista se vacio");
-}
+//////////////////////////////////////////////////////////////////////
 
-// Borrar nodo de la lista
-void deleteNode(int date, doubleLinkedList *lista)
+
+
+int main()
 {
 
-    nodeDL *node = lista->head;
-    if (lista->size > 0) // Verificamos que la lista o el dato no esten vacios
-    {
-        while (node != NULL) // Mientras  llegue a la cola y el siguiente a el sea nulo
-        {
+    // Carga la materia de todas las carreras
+    cargarMaterias();
 
-            if (node->date == date)
-            {
-                if (node->prev != NULL)
-                {
-                    node->prev->next = node->next;
-                }
-                else
-                {
-                    lista->head = node->next; // Es el primer nodo
-                }
-                if (node->next != NULL)
-                {
 
-                    node->next->prev = node->prev;
-                }
-                else
-                {
-                    lista->tail = node->prev; // Es el último nodo
-                }
 
-                free(node); // libero memoria en nodo ya que encontro el valor y lo elimino
-                node = NULL;
-                lista->size--;
-                printf("Valor eliminado con exito");
-                return;
-            }
-
-            node = node->next;
-        }
-    }
-    printf("Nodo con valor %d no encontrado.\n", date);
-}
-
-nodeDL *findNode(int date, doubleLinkedList *lista)
-{
-    nodeDL *node = lista->head;
-    if (lista->size > 0) //Verificamos que existe una lista
-    {
-
-        while (node != NULL)
-        {
-            if (node->date == date)
-            {
-                printf("El valor del nodo encontrado: %d ", node->date);
-                return node;
-            }
-
-            node = node->next;
-        }
-    }
-    printf("Valor del nodo no encontrado");
-    return NULL;
-}
-void main()
-{
-    doubleLinkedList lista = {NULL, NULL, 0};
     int opc = 0;
-    int data;
     int flag = 0;
-    int inicioFinal;
 
     while (flag == 0)
     {
-        printf("\nIngrese una opcion entre las posibles: 1-Agregar 2-Eliminar 3-Buscar 4-Imprimir 5-Limpiar 6-Salir: ");
+
+        printf("\nQue desea hacer? 1- Cargar Estudiante 2- Cargar Nueva Materia 3- Mostrar Lista 4- Ver materias del plan 5- Salir: \n ");
         scanf("%d", &opc);
-
-    switch (opc)
+        if (opc < 1 || opc > 5)
         {
-            
-        case 1:
-
-            printf("Ingrese el valor del nodo: ");
-            scanf("%d", &data);
-            printf("Donde desea agregar? 1- Inicio 2- Final: ");
-            scanf("%d", &inicioFinal);
-            if (inicioFinal == 1)
-            {
-                prepend(&lista, data);
-                data = 0;
-                break;
-            }
-            else if (inicioFinal == 2)
-            {
-                append(&lista, data);
-                data = 0;
-                break;
-            
-        }else{
-            printf("Opcion inexistente.");
-            break;
+            printf("Opcion no valida, por favor ingrese un valor entre 1 y 3.\n");
+            continue;
         }
-            printf("Valor ingresado con exito.");
 
-        case 2:
-
-            printf("Ingrese el valor del nodo a eliminar: ");
-            scanf("%d", &data);
-            deleteNode(data, &lista);
-            data = 0;
+        switch (opc)
+        {
+        case 1:
+            cargarEstudiante();
             break;
 
         case 3:
-            printf("Ingrese el valor del nodo para buscar en la lista: ");
-            scanf("%d", &data);
-            nodeDL *nodoBuscado;
-            nodoBuscado = findNode(data, &lista);
-       
+                printList(&lista);
+
             break;
 
-        case 4:
-            printList(&lista);
-            break;
+           case 4:
+                printMaterias();
 
+            break;
         case 5:
-            clear(&lista);
-            break;
-
-        case 6:
-            printf("Muchas gracias por usar este programa! Lo esperamos pronto.\n");
+            opc = 0;
+            clear(&lista); // Limpia la lista de estudiantes al salir del programa
+            clearMaterias(); // Limpia las materias de memoria al salir del programaaa
+            printf("\nMuchas gracias por usar este programa! Lo esperamos pronto.\n");
             flag = 1;
-            break;
+        break;
 
         default:
             printf("Opcion no encontrada, por favor ingrese otro valor.\n");
             break;
         }
-}
     }
+    return 0;
+}
+
+//     // doubleLinkedList lista = {NULL, NULL, 0};
+//     int opc = 0;
+//     int data;
+//     int flag = 0;
+//     int inicioFinal;
+
+//     while (flag == 0)
+//     {
+//         printf("\nIngrese una opcion entre las posibles: 1-Agregar 2-Eliminar 3-Buscar 4-Imprimir 5-Limpiar 6-Salir: ");
+//         scanf("%d", &opc);
+
+//     switch (opc)
+//         {
+
+//         case 1:
+
+//             printf("Ingrese el valor del nodo: ");
+//             scanf("%d", &data);
+//             printf("Donde desea agregar? 1- Inicio 2- Final: ");
+//             scanf("%d", &inicioFinal);
+//             if (inicioFinal == 1)
+//             {
+//                 prepend(&lista, data);
+//                 data = 0;
+//                 break;
+//             }
+//             else if (inicioFinal == 2)
+//             {
+//                 append(&lista, data);
+//                 data = 0;
+//                 break;
+
+//         }else{
+//             printf("Opcion inexistente.");
+//             break;
+//         }
+//             printf("Valor ingresado con exito.");
+
+//         case 2:
+
+//             printf("Ingrese el valor del nodo a eliminar: ");
+//             scanf("%d", &data);
+//             deleteNode(data, &lista);
+//             data = 0;
+//             break;
+
+//         case 3:
+//             printf("Ingrese el valor del nodo para buscar en la lista: ");
+//             scanf("%d", &data);
+//             nodeDL *nodoBuscado;
+//             nodoBuscado = findNode(data, &lista);
+
+//             break;
+
+//         case 4:
+//             printList(&lista);
+//             break;
+
+//         case 5:
+//             clear(&lista);
+//             break;
+
+//         case 6:
+//             printf("Muchas gracias por usar este programa! Lo esperamos pronto.\n");
+//             flag = 1;
+//             break;
+
+//         default:
+//             printf("Opcion no encontrada, por favor ingrese otro valor.\n");
+//             break;
+//         }
+// }
+
+
+
+
+////////////////////////CARGAR ESTUDIANTES////////////////////////////////////
+
+
+void cargarEstudiante()
+{
+    int fechaEsValida = 0;
+    Estudiante *estudiante = malloc(sizeof(Estudiante));
+    if (estudiante == NULL)
+    {
+        printf("Error al reservar memoria para el estudiante.\n");
+        return;
+    }
+
+    printf("Ingrese el nombre del estudiante: ");
+    scanf("%s", estudiante->nombre);
+    printf("Ingrese la fecha de nacimiento del estudiante (DD/MM/AAAA): ");
+    scanf("%s", estudiante->nacimiento);
+
+    fechaEsValida = validarFecha(estudiante->nacimiento);
+  
+
+   if(fechaEsValida == 0)
+    {
+        printf("Fecha de nacimiento invalida. Por favor, ingrese una fecha en formato DD/MM/AAAA.\n");
+        free(estudiante); // libero toda la memoria reservada para el estudiante ya que ingreso mal un dato.
+        return;
+    }
+
+    printf("Ingrese el DNI del estudiante: ");
+    scanf("%d", &estudiante->dni);
+    if (estudiante->dni < 10000000 || estudiante->dni > 99999999)
+    {
+        printf("DNI invalido. Debe tener 8 digitos.\n");
+        free(estudiante); // libero toda la memoria reservada para el estudiante ya que ingreso mal un dato.
+        return;
+    }
+    printf("Ingrese el Legajo del estudiante: ");
+    scanf("%d", &estudiante->legajo);
+
+    append(&lista, estudiante);
+    printf("Estudiante cargado con exito:");
+}
+
+
+
+////////////////////////////////////////////////////////////////////
+
+
+// funcion para mostrar Materias del plan de estudio
+void printMaterias()
+{
+    printf("\nMaterias del plan de estudio:\n");
+    printf("\n-------------------------------\n");
+
+    for (int i = 0; i < CANT_MATERIAS; i++)
+    {
+        printf("%s\n", materias[i]->nombreMateria);
+    }
+        printf("\n-------------------------------\n");
+
+}
+
+clearMaterias()
+{
+    for (int i = 0; i < CANT_MATERIAS; i++)
+    {
+        free(materias[i]); // Libera la memoria para cada materia
+    }
+}
+
+
+
+// No valida por años biciestos
+int validarFecha(char *fecha)
+{
+    char copia[11];
+    strcpy(copia, fecha); // strtok modifica la cadena original, por eso copiamos
+
+    // Usamos strtok como un split
+    char *diaStr = strtok(copia, "/");
+    char *mesStr = strtok(NULL, "/");
+    char *anioStr = strtok(NULL, "/");
+
+    if (diaStr == NULL || mesStr == NULL || anioStr == NULL)
+    {
+        return 0; // Formato inválido
+    }
+    // Convertimos las cadenas a enteros
+    int dia = atoi(diaStr);
+    int mes = atoi(mesStr);
+    int anio = atoi(anioStr);
+
+    if (anio < 1900 || anio > 2100)
+    {
+        return 0; // Año fuera de rango
+    }
+    if (mes < 1 || mes > 12)
+    {
+        return 0; // Mes fuera de rango
+    }
+    if (dia < 1 || dia > 31)
+    {
+        return 0; // Día fuera de rango
+    }
+    
+    if((obtenerAnioActual() - anio) < 18)
+    {
+        printf("El estudiante debe ser mayor de edad.\n");
+        return 0; // El estudiante debe ser mayor de edad
+    }
+    return 1; // Placeholder, siempre retorna válido
+}
+
+
+
+//Usa libreria time.h para obtener el año actual
+int obtenerAnioActual() {
+    time_t t = time(NULL);              // Obtiene el tiempo actual
+    struct tm *tm_info = localtime(&t); // Lo convierte a estructura local
+
+    int anio = tm_info->tm_year + 1900; // Año desde 1900 Porque tm_year representa el número de años desde 1900, entonces: se debe sumar 1900
+    
+
+    return anio; // Retorna el año actual
+}
+
+#endif;
